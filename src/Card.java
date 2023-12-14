6af1ee585899f520ad9fc4d45d06528ccff435b8
Nathaniel Peyer <101201931+NatePeyer@users.noreply.github.com>
2023-12-13 21:16:46 -0600

    set up look up tables in card class

--- src/Card.java
+++ src/Card.java
@@ -1,11 +1,57 @@
 public class Card
 {
-    private String suit;
-    private String type;
+    public enum Suit 
+    {
+        CLUBS,
+        DIAMONDS,
+        HEARTS,
+        SPADES,
+        _LENGTH
+    };
+    private Suit suit;
+    private int value;
 
-    public Card(String suitIn, String typeIn)
+    public Card(Suit suitIn, int valueIn)
     {
         suit = suitIn;
-        type = typeIn;
+        value = valueIn;
     }
+
+    public String toString()
+    {
+        final String CARDNAME[] = 
+        {
+            "None", //0
+            "Ace", //1
+            "2", //2
+            "3", //3
+            "4", //4
+            "5", //5
+            "6", //6
+            "7", //7
+            "8", //8
+            "9", //9
+            "10", //10
+            "Jack", //11
+            "Queen", //12
+            "King" //13
+        };
+        final String CARDSUIT[_LENGTH] =
+        {
+            "Clubs",
+            "Diamonds",
+            "Hearts",
+            "Spades"
+        };
+
+        if((suit < 0) || (suit >= _LENGTH))
+        {
+            return "invalid card suit";
+        }
+        if((value < 1) || (value >= CARDNAME.length))
+        {
+            return "invalid card value";
+        }
+        return CARDNAME[value] + " of " + CARDSUIT[suit];
+    } 
 }
