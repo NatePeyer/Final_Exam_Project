1b1ae1e7e8feb8edf816c77ac1af78cdff6a5076
Nate Peyer <nathaniel.peyer24@d155.org>
2023-12-14 21:09:35 -0600

    Finished debugging. Added shuffle method.

--- src/DeckOfCards.java
+++ src/DeckOfCards.java
@@ -1,21 +1,52 @@
+import java.lang.Math;
 import java.util.*;
 
 public class DeckOfCards {
     public Card deck[] = new Card[52];
-    private String typeIn = "";
-    private String suitIn = "";
 
     public DeckOfCards(Card cardIn) {
         int i = 0;
-        for (int j = 0; j < 4; j++)
+        for (Card.Suit j : Card.Suit.values())
             {
                 for (int k = 1; k <= 13; k++)
                 {
-                    deck[i] = new Card(Suit(j),k);
+                    deck[i] = new Card(j,k);
                     i++;
                 }
+            }
+    }
 
+    public void shuffle()
+    {
+        boolean run = true;
+        int count = 0;
+        Card temp;
+
+        while(run)
+        {
+            if(count == 2)
+            {
+                break;
             }
 
+            for(int i = 0; i < deck.length; i++)
+            {
+                int ranSpot = (int)(Math.random() * 52);
+                temp = deck[ranSpot];
+                deck[ranSpot] = deck[i];
+                deck[i] = temp;
+            }
+            count++;
+        }
+    }
+    
+    public String toString()
+    {
+        String returnString = "";
+        for(int i = 0; i < deck.length; i++)
+        {
+            returnString += deck[i].toString() + "\n";
+        }
+        return returnString;
     }
 }
